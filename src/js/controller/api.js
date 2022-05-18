f2fa236130a6d503e348e833022d92107bbe0c75
Cayo Nakasato <cayonakasato112@gmail.com>
2022-05-18 19:00:59 -0400

    feat: create requisitions from api to login and cadaster user

---
+++ src/js/controller/api.js
@@ -0,0 +1,44 @@
+import Login from "../JS/Login.js"
+import Register from "../JS/Register.js"
+
+class Api{
+    static BASIC__URL = "https://api-kenzie-food.herokuapp.com"
+    static registerUser(data){
+        fetch(
+            this.BASIC__URL + "/auth/register",
+        {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then((res) => {
+            if(res.status === "Error" || res.status !== undefined){
+                Register.registerErrorModal(res.message)
+            }else{
+                Login.loginModal()
+            }
+        })
+    }
+
+    static logarUsuario(data){
+        fetch(
+            this.BASIC__URL + "/auth/login",
+            {
+                method: "POST",
+                headers:{
+                    "Content-Type" : "application/json"
+                },
+                body: JSON.stringify(data)
+            }
+        )
+        .then(res => res.json())
+        .then(()=>{
+
+        })
+    }
+}
+
+export default Api
