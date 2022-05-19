c84205b37e80ad8bd187213e7fcde71063ffb37e
GabrielleMedeirosOliveira <gabriellemedeiros51@gmail.com>
2022-05-19 08:45:26 -0300

    creted: showCase and filters with DOM

---
+++ src/js/controller/apiRequest.js
@@ -0,0 +1,15 @@
+class ApiRequest {
+    static baseUrl = 'https://api-kenzie-food.herokuapp.com/'
+    static async requisition(){
+        const response = await fetch(this.baseUrl+"products", {
+            method: "GET", 
+            headers: {
+              "Content-Type": "application/json", 
+            },
+        })
+        .then(res => res.json())
+        .then(data => data)
+        return response
+    }
+  }
+  export {ApiRequest}
---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+import { ApiRequest } from "./controller/apiRequest.js";
+import {showCase} from './models/showCaseDOM.js';
+showCase.homePage()
+showCase.displayProducts()
